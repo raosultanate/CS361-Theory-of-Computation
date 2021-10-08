a892c98bfd784911a4681c7738f22ceb97753ae5
raosultanate <76628973+raosultanate@users.noreply.github.com>
2021-10-08 14:28:33 -0600

    progress #1

--- P1/fa/dfa/DFA.java
+++ P1/fa/dfa/DFA.java
@@ -4,29 +4,85 @@ import java.util.Set;
 
 import fa.State;
 
+import java.util.Set;
+import java.util.LinkedHashSet;
+
 public class DFA implements fa.dfa.DFAInterface{
+
+    // Set of Final States. (F)
+    private Set<DFAState> finalStateSet;
+    // Start state of DFA. (q0)
+    // Not a Set because there can be only 1 initial State in a FA.
+    private DFAState initialState;
+    //Set of Rest of the states (Q)
+    private Set<DFAState> stateSet;
+    //Set of all the characters. (Sigma)
+    private Set<Character> alphabetSet;
+
+    //Constructor for DFA
+    public DFA(){
+        // There can be more than one final states.
+        finalStateSet = new LinkedHashSet<>();
+        // There can only be one initial states.
+        // However, initlally there will be no state at all when first constructed.
+        initialState = null;
+        // This list keeps track of all the states.
+        stateSet = new LinkedHashSet<>();
+        // these are all the input characters.
+        alphabetSet = new LinkedHashSet<>();
+
+    }
 
     @Override
     public void addStartState(String name) {
-        // TODO Auto-generated method stub
+        // CHeck if the initial state has already been added to the list of all states.
+        // The reason we check to make sure is because there can only be 1 initial state present.
+        for(DFAState s : stateSet ) {
+            if(s.getName() == name) {
+                initialState = s;
+                return;
+            }
+        }
+
+        // If initlial state not preseent in the list of state's than this code is run
+        // We create a new state and than add it to the list of states.
+        initialState = new DFAState(name);
+        // finally add it to the list of all the states.
+        stateSet.add(initialState);
         
     }
 
     @Override
     public void addState(String name) {
-        // TODO Auto-generated method stub
+        // Create a new state and add it to the set of states.
+        DFAState newState = new DFAState(name);
+        stateSet.add(newState);	
         
     }
 
     @Override
     public void addFinalState(String name) {
-        // TODO Auto-generated method stub
+        // There is no need to check here because we can have multiple final states.
+        // Create the new final state here and than add it to the list of all the states.
+        DFAState finalState = new DFAState(name);
+        // Once created add it to the list of all the states and also add it to the list of final States.
+        // Adding to the list of all the states.
+        stateSet.add(finalState);
+        //Adding to the list of finalState.
+        finalStateSet.add(finalState);
         
     }
 
     @Override
     public void addTransition(String fromState, char onSymb, String toState) {
-        // TODO Auto-generated method stub
+        // Right away add the onSymb to the set of characters
+        alphabetSet.add(onSymb);
+
+        // Now we iterate over the set of states to build the hashMap in teh DFAState.java class
+        // This is equivalent to building transition table.
+
+        //We must get the fromState in order to get to the toState and build the transition.
+
         
     }
 
--- P1/fa/dfa/DFAState.java
+++ P1/fa/dfa/DFAState.java
@@ -1,5 +1,33 @@
 package fa.dfa;
 
+import java.util.HashMap;
+
 public class DFAState extends fa.State {
+
+    // HashMap will hold all the transitions
+    private HashMap<Character, DFAState> transitions;
+
+
+    // Each DFAState will keep track of the next DFAState with the help of the character (as key) in its hashMap.
+    public DFAState(String name) {
+        //name variable was present in the fa.State.
+        this.name = name;
+        // initializing the dataStructure to hold all the transitions
+        transitions = new HashMap<Character, DFAState>();
+    }
+
+    // record data about Cuttent State to the certain state using a
+    // character/Symbol.
+    public void toNextState(char character, DFAState state) {
+        // officially adding the state to the hashMap,
+        // each DFA state keeps track of the next state using the character.
+        transitions.put(character, state);
+    }
+
+    // Returns the next state if the mapping is present to that state in the HashMap. If
+    // not than returns a null.
+    public DFAState getNextState(char character) {
+        return transitions.get(character);
+    }
 
 }
