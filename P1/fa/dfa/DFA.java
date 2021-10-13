f3dea68fbabefe7f0f7abec19eed8b003498988a
raosultanate <76628973+raosultanate@users.noreply.github.com>
2021-10-12 19:09:35 -0600

    Added additional implementations

--- P1/fa/dfa/DFA.java
+++ P1/fa/dfa/DFA.java
@@ -3,24 +3,25 @@ package fa.dfa;
 import java.util.Set;
 
 import fa.State;
+import fa.FAInterface;
 
 import java.util.Set;
 import java.util.LinkedHashSet;
 
-public class DFA implements fa.dfa.DFAInterface{
+public class DFA implements fa.dfa.DFAInterface {
 
     // Set of Final States. (F)
     private Set<DFAState> finalStateSet;
     // Start state of DFA. (q0)
     // Not a Set because there can be only 1 initial State in a FA.
     private DFAState initialState;
-    //Set of Rest of the states (Q)
+    // Set of Rest of the states (Q)
     private Set<DFAState> stateSet;
-    //Set of all the characters. (Sigma)
+    // Set of all the characters. (Sigma)
     private Set<Character> alphabetSet;
 
-    //Constructor for DFA
-    public DFA(){
+    // Constructor for DFA
+    public DFA() {
         // There can be more than one final states.
         finalStateSet = new LinkedHashSet<>();
         // There can only be one initial states.
@@ -35,10 +36,11 @@ public class DFA implements fa.dfa.DFAInterface{
 
     @Override
     public void addStartState(String name) {
-        // CHeck if the initial state has already been added to the list of all states.
-        // The reason we check to make sure is because there can only be 1 initial state present.
-        for(DFAState s : stateSet ) {
-            if(s.getName() == name) {
+        // Check if the initial state has already been added to the list of all states.
+        // The reason we check to make sure is because there can only be 1 initial state
+        // present.
+        for (DFAState s : stateSet) {
+            if (s.getName() == name) {
                 initialState = s;
                 return;
             }
@@ -49,28 +51,40 @@ public class DFA implements fa.dfa.DFAInterface{
         initialState = new DFAState(name);
         // finally add it to the list of all the states.
         stateSet.add(initialState);
-        
+
     }
 
     @Override
     public void addState(String name) {
         // Create a new state and add it to the set of states.
         DFAState newState = new DFAState(name);
-        stateSet.add(newState);	
-        
+        stateSet.add(newState);
+
     }
 
     @Override
     public void addFinalState(String name) {
         // There is no need to check here because we can have multiple final states.
-        // Create the new final state here and than add it to the list of all the states.
+        // Create the new final state here and than add it to the list of all the
+        // states.
         DFAState finalState = new DFAState(name);
-        // Once created add it to the list of all the states and also add it to the list of final States.
+        // Once created add it to the list of all the states and also add it to the list
+        // of final States.
         // Adding to the list of all the states.
         stateSet.add(finalState);
-        //Adding to the list of finalState.
+        // Adding to the list of finalState.
         finalStateSet.add(finalState);
-        
+
+    }
+
+    // Returns the state if it exists in the stateSet
+    public DFAState get_DFAState(String name) {
+        for (DFAState s : stateSet) {
+            if (s.getName() == name) {
+                return s;
+            }
+        }
+        return null;
     }
 
     @Override
@@ -78,47 +92,75 @@ public class DFA implements fa.dfa.DFAInterface{
         // Right away add the onSymb to the set of characters
         alphabetSet.add(onSymb);
 
-        // Now we iterate over the set of states to build the hashMap in teh DFAState.java class
+        // Now we iterate over the set of states to build the hashMap
         // This is equivalent to building transition table.
 
-        //We must get the fromState in order to get to the toState and build the transition.
+        for (DFAState s : stateSet) {
+            if (s.getName() == fromState) {
+                // Set the transition here. From this state to the other state.
+                s.toNextState(onSymb, get_DFAState(toState));
 
-        
+            }
+        }
+
     }
 
     @Override
     public Set<? extends State> getStates() {
-        // TODO Auto-generated method stub
-        return null;
+        // Return the list of all states as set.
+        return stateSet;
     }
 
     @Override
     public Set<? extends State> getFinalStates() {
-        // TODO Auto-generated method stub
-        return null;
+        // Return the list of all the final states as set.
+        return finalStateSet;
     }
 
     @Override
     public State getStartState() {
-        // TODO Auto-generated method stub
-        return null;
+        // return the initial state as a DSAState.
+        return initialState;
     }
 
     @Override
     public Set<Character> getABC() {
-        // TODO Auto-generated method stub
-        return null;
+        // returns the list of all characters as a set.
+        return alphabetSet;
     }
 
     @Override
     public boolean accepts(String s) {
-        // TODO Auto-generated method stub
-        return false;
+        // We need to check if the string is accepted or not as completely or rejected.
+        // We need a pointer which will start from current state, and will move on from
+        // there as we iterate over the string.
+        int stringLength = s.length();
+        DFAState currentState = initialState;
+        boolean stringAccepted = false;
+
+        int i = 0;
+        while (i < stringLength) {
+            // keeps going to next state as it iterates over the string.
+            currentState = getToState(currentState, s.charAt(i));
+            i++;
+        }
+        // check after iteration if currentState is in the set of final state.
+        if (finalStateSet.contains(currentState)) {
+            stringAccepted = true;
+        }
+
+        return stringAccepted;
     }
 
     @Override
-    public State getToState(DFAState from, char onSymb) {
-        // TODO Auto-generated method stub
+    public DFAState getToState(DFAState from, char onSymb) {
+        // Used during transition. If transition exists on that symbol than state is
+        // returned else returns null.
+        return from.getNextState(onSymb);
+    }
+
+    @Override
+    public String toString() {
         return null;
     }
 
--- P1/fa/dfa/DFAState.java
+++ P1/fa/dfa/DFAState.java
@@ -7,10 +7,10 @@ public class DFAState extends fa.State {
     // HashMap will hold all the transitions
     private HashMap<Character, DFAState> transitions;
 
-
-    // Each DFAState will keep track of the next DFAState with the help of the character (as key) in its hashMap.
+    // Each DFAState will keep track of the next DFAState with the help of the
+    // character (as key) in its hashMap.
     public DFAState(String name) {
-        //name variable was present in the fa.State.
+        // name variable was present in the fa.State.
         this.name = name;
         // initializing the dataStructure to hold all the transitions
         transitions = new HashMap<Character, DFAState>();
@@ -24,7 +24,8 @@ public class DFAState extends fa.State {
         transitions.put(character, state);
     }
 
-    // Returns the next state if the mapping is present to that state in the HashMap. If
+    // Returns the next state if the mapping is present to that state in the
+    // HashMap. If
     // not than returns a null.
     public DFAState getNextState(char character) {
         return transitions.get(character);
